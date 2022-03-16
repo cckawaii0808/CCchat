5f1576e4ad2385b425511ccf9c98f5a39c07128a
cc <cc_xie@gmail.com>
2022-03-16 15:18:31 +0800

    websocket 連接完成

--- app/src/main/java/com/ccplay/ccchat/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ccplay/ccchat/ui/home/HomeFragment.kt
@@ -1,38 +1,80 @@
 package com.ccplay.ccchat.ui.home
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.ccplay.ccchat.databinding.FragmentHomeBinding
+import com.google.gson.Gson
+import okhttp3.*
+import okio.ByteString
+import java.util.concurrent.TimeUnit
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
-
+    lateinit var websocket: WebSocket//使用插件
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+      /*  binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }*/
+        //Web socket
+        val client = OkHttpClient.Builder()
+            .readTimeout(3, TimeUnit.SECONDS)
+            .build()
+        val request = Request.Builder()
+            .url("wss://lott-dev.lottcube.asia/ws/chat/chat:app_test?nickname=Hank")
+            .build()
+        websocket = client.newWebSocket(request, object : WebSocketListener() {
+            override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+                super.onClosed(webSocket, code, reason)
+                Log.d(TAG, ": onClosed");
+            }
+
+            override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+                super.onClosing(webSocket, code, reason)
+                Log.d(TAG, ": onClosing");
+            }
+
+            override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                super.onFailure(webSocket, t, response)
+                Log.d(TAG, ": onFailure");
+            }
+
+            override fun onMessage(webSocket: WebSocket, text: String) {
+                super.onMessage(webSocket, text)
+                Log.d(TAG, ": onMessage $text");
+            }
+
+            override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+                super.onMessage(webSocket, bytes)
+                Log.d(TAG, ": onMessage ${bytes.hex()}");
+            }
+
+            override fun onOpen(webSocket: WebSocket, response: Response) {
+                super.onOpen(webSocket, response)
+                Log.d(TAG, ": onOpen");
+//                webSocket.send("Hello, I am Hank")
+            }
+        })
+       /* binding.bSend.setOnClickListener {
+            val message = binding.edMessage.text.toString()
+            websocket.send(Gson().toJson(Message("N", message)))
+        }*/
     }
 
     override fun onDestroyView() {
@@ -40,3 +82,6 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 }
+
+data class Message(val action:String, val content: String)
+
