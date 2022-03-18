b2fdb2fe5bcd73186d836b4dfd3e34b3dfdb2d5d
cc <cc_xie@gmail.com>
2022-03-18 09:18:46 +0800

    今天星期五我快完蛋 但他可以跳轉註冊了

--- app/src/main/java/com/ccplay/ccchat/ui/Profife/Signup_Fragment.kt
+++ app/src/main/java/com/ccplay/ccchat/ui/Profife/Signup_Fragment.kt
@@ -1,5 +1,4 @@
 package com.ccplay.ccchat.ui.Profife
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,22 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import com.ccplay.ccchat.R
 import com.ccplay.ccchat.databinding.FragmentProfileBinding
+import com.ccplay.ccchat.databinding.FragmentSignupBinding
 
 
 class Signup_Fragment : Fragment() {
-    private var _binding: FragmentProfileBinding? = null
+    private var _binding: FragmentSignupBinding? = null
     private val binding get() = _binding!!
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
+      return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val nickname = binding.tvSignupNickname.text.toString()
+        val username = binding.tvSignupUsername.text.toString()
+        val password = binding.tvSignupPassword.text.toString()
     }
 
 }
